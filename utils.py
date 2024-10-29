a8fb8ee62ef7322c6d0cad12ad91d5d90b041f11
cloner174 <hamedhajipour1400@gmail.com>
2024-10-29 12:04:50 +0330

    add circle mask mark

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 import torch
+import cv2
+import numpy as np
+
 
 
 def visualize_predictions(model, dataloader, device, num_samples=5, num_classes=1):
@@ -7,7 +10,6 @@ def visualize_predictions(model, dataloader, device, num_samples=5, num_classes=
     model.eval()
     images_shown = 0
     plt.figure(figsize=(20, num_samples * 5))
-    
     with torch.no_grad():
         for batch in dataloader:
             if len(batch) == 4:
@@ -44,7 +46,6 @@ def visualize_predictions(model, dataloader, device, num_samples=5, num_classes=
                     true_box = None
                     true_label = 1
                 
-                
                 pred_cls = preds_cls[i].item()
                 
                 if int(pred_cls) == 1 :
@@ -54,7 +55,6 @@ def visualize_predictions(model, dataloader, device, num_samples=5, num_classes=
                     pred_mask = preds_seg[i].cpu().numpy()
                 
                 pred_box = preds_loc[i].cpu().numpy()
-                
                 
                 plt.subplot(num_samples, 6, images_shown * 6 + 1)
                 plt.imshow(img, cmap='gray')
@@ -111,4 +111,108 @@ def visualize_predictions(model, dataloader, device, num_samples=5, num_classes=
     
     plt.tight_layout()
     plt.show()
+
+
+
+def pro_prediction(model, dataloader, device, num_samples=5):
+    model.eval()
+    images_shown = 0
+    plt.figure(figsize=(20, num_samples * 5))
+    with torch.no_grad():
+        for batch in dataloader:
+            if len(batch) == 4:
+                images, masks, labels, boxes = batch
+                is_annotated = True
+            else:
+                images, labels = batch
+                is_annotated = False
+            
+            images = images.to(device)
+            
+            if is_annotated:
+                masks = masks.to(device)
+                labels = labels.to(device)
+            
+            outputs_seg, outputs_cls, _ = model(images)
+            
+            preds_seg = torch.argmax(outputs_seg, dim=1)
+            preds_cls = (torch.sigmoid(outputs_cls) > 0.5).float()
+            
+            for i in range(images.size(0)):
+                if images_shown >= num_samples:
+                    break
+                
+                img = images[i].cpu().squeeze().numpy()
+                
+                ax_img = plt.subplot(num_samples, 6, images_shown * 6 + 1)
+                ax_img.imshow(img, cmap='gray')
+                ax_img.set_title('Input Image')
+                ax_img.axis('off')
+                if is_annotated:
+                    true_mask = masks[i].squeeze().cpu().numpy()
+                    true_label = labels[i].item()
+                else:
+                    true_label = 1
+                
+                pred_cls = preds_cls[i].item()
+                
+                if int(pred_cls) == 1:
+                    pred = torch.sigmoid(outputs_seg[i])
+                    pred_mask = pred.squeeze().cpu().numpy()
+                else:
+                    pred_mask = preds_seg[i].cpu().numpy()
+
+                ax_cls = plt.subplot(num_samples, 6, images_shown * 6 + 4)
+                if is_annotated:
+                    ax_cls.text(0.1, 0.5, f'True: {int(true_label)}\nPred: {int(pred_cls)}',
+                                fontsize=12, bbox=dict(facecolor='white', alpha=0.5))
+                else:
+                    ax_cls.text(0.1, 0.5, f'Pred: {int(pred_cls)}',
+                                fontsize=12, bbox=dict(facecolor='white', alpha=0.5))
+                
+                ax_cls.set_title('Classification')
+                ax_cls.axis('off')
+
+                if is_annotated:
+                    ax_true_circle = plt.subplot(num_samples, 6, images_shown * 6 + 2)
+                    ax_true_circle.imshow(img, cmap='gray')
+                    ax_true_circle.set_title('True Mask & Circles')
+                    ax_true_circle.axis('off')
+                    
+                    mask_binary = (true_mask > 0.5).astype(np.uint8) * 255
+                    contours, _ = cv2.findContours(mask_binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                    
+                    for contour in contours:
+                        if len(contour) > 0:
+                            (x, y), radius = cv2.minEnclosingCircle(contour)
+                            center = (int(x), int(y))
+                            radius = int(radius)
+                            circle = plt.Circle(center, radius, edgecolor='blue', facecolor='none', lw=0.7)
+                            ax_true_circle.add_patch(circle)
+                
+                ax_pred_circle = plt.subplot(num_samples, 6, images_shown * 6 + 3)
+                ax_pred_circle.imshow(img, cmap='gray')
+                ax_pred_circle.set_title('Pred Mask & Circles')
+                ax_pred_circle.axis('off')
+                
+                pred_mask_binary = (pred_mask > 0.5).astype(np.uint8) * 255
+                contours, _ = cv2.findContours(pred_mask_binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                
+                for contour in contours:
+                    if len(contour) > 0:
+                        (x, y), radius = cv2.minEnclosingCircle(contour)
+                        center = (int(x), int(y))
+                        radius = int(radius)
+                        circle = plt.Circle(center, radius, edgecolor='red', facecolor='none', lw=0.7)
+                        ax_pred_circle.add_patch(circle)
+                
+                images_shown += 1
+            
+            if images_shown >= num_samples:
+                break
+    
+    plt.tight_layout()
+    plt.show()
+
+
 #cloner174
